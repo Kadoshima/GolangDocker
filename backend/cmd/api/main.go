f60aa53b67f79686969e627fdc132823d81a17b1
Kadoshima hagihara <kadoshima_hagihara@crystal-tec.co.jp>
2024-07-11 14:57:37 +0900

    [add] air

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	http.HandleFunc("/", helloHandler)
+	http.ListenAndServe(":8080", nil)
 }
