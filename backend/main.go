d45e36f61dc75f5f6e48eb7a6d0a17d5596dafac
Kadoshima hagihara <kadoshima_hagihara@crystal-tec.co.jp>
2024-08-04 10:42:59 +0900

    [fix] docker file関連を修正

--- backend/main.go
+++ backend/main.go
@@ -9,8 +9,8 @@ import (
 func main() {
 	r := router.SetupRouter()
 
-	log.Println("Starting server on :8080")
-	if err := http.ListenAndServe(":8080", r); err != nil {
+	log.Println("Starting server on :8000")
+	if err := http.ListenAndServe(":8000", r); err != nil {
 		log.Fatalf("Could not start server: %v", err)
 	}
 }
