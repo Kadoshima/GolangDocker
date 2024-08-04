e64b47fa8f9e098173407211a749d36a4131f0c7
Kadoshima hagihara <kadoshima_hagihara@crystal-tec.co.jp>
2024-08-04 12:30:26 +0900

    [add] DB管理のためphpmyadminを追加

---
+++ backend/infrastructure/database/database.go
@@ -0,0 +1,25 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func NewDB() (*sql.DB, error) {
+	dsn := "root:pass@tcp(127.0.0.1:3306)/test_db" // テスト用なのでgithubに挙げて問題なし
+	db, err := sql.Open("mysql", dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// データベース接続を確認
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	log.Println("Connected to the database!")
+	return db, nil
+}
