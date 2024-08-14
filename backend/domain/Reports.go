b1049a037b823bf261bfddc1d4cea7dc76587cc1
Kadoshima hagihara <kadoshima_hagihara@crystal-tec.co.jp>
2024-08-14 23:27:35 +0900

    [delete] test

--- backend/domain/Reports.go
+++ backend/domain/Reports.go
@@ -1,14 +1,8 @@
 package domain
 
-import "time"
-
 type Report struct {
 	Id           int
 	ReportedById int
 	ForumId      int
 	PostId       int
-	Reason       string
-	ReportedAt   time.Time
-	CreatedAt    time.Time
-	UpdatedAt    time.Time
 }
